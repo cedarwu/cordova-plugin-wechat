6a42b91825ae247d552da558ed1e0b40e863654a
yuanspan <yuanspan@tencent.com>
2025-06-18 16:52:14 +0800

    修复import失败的报错

--- src/android/Wechat.java
+++ src/android/Wechat.java
@@ -14,6 +14,7 @@ import android.webkit.URLUtil;
 import com.tencent.mm.opensdk.modelbiz.ChooseCardFromWXCardPackage;
 import com.tencent.mm.opensdk.modelbiz.WXLaunchMiniProgram;
 import com.tencent.mm.opensdk.modelbiz.WXOpenBusinessWebview;
+import com.tencent.mm.opensdk.modelbiz.WXOpenBusinessView;
 import com.tencent.mm.opensdk.modelmsg.SendAuth;
 import com.tencent.mm.opensdk.modelmsg.SendMessageToWX;
 import com.tencent.mm.opensdk.modelmsg.WXAppExtendObject;
